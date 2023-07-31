a3acf482e5766267e9e5bfa07bbf02ae274c70c8
KKcorps <kharekartik@gmail.com>
2023-07-31 14:17:40 +0530

    WIP: multitable test

--- pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/UpsertTableSegmentPreloadIntegrationTest.java
+++ pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/UpsertTableSegmentPreloadIntegrationTest.java
@@ -21,6 +21,7 @@ package org.apache.pinot.integration.tests;
 import com.fasterxml.jackson.databind.JsonNode;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -31,13 +32,19 @@ import org.apache.pinot.common.utils.HashUtil;
 import org.apache.pinot.common.utils.LLCSegmentName;
 import org.apache.pinot.common.utils.helix.HelixHelper;
 import org.apache.pinot.server.starter.helix.BaseServerStarter;
+import org.apache.pinot.spi.config.table.ColumnPartitionConfig;
+import org.apache.pinot.spi.config.table.ReplicaGroupStrategyConfig;
+import org.apache.pinot.spi.config.table.RoutingConfig;
+import org.apache.pinot.spi.config.table.SegmentPartitionConfig;
 import org.apache.pinot.spi.config.table.TableConfig;
 import org.apache.pinot.spi.config.table.TableType;
+import org.apache.pinot.spi.config.table.UpsertConfig;
 import org.apache.pinot.spi.data.Schema;
 import org.apache.pinot.spi.env.PinotConfiguration;
 import org.apache.pinot.spi.utils.CommonConstants;
 import org.apache.pinot.spi.utils.CommonConstants.Helix.StateModel.SegmentStateModel;
 import org.apache.pinot.spi.utils.JsonUtils;
+import org.apache.pinot.spi.utils.builder.TableConfigBuilder;
 import org.apache.pinot.spi.utils.builder.TableNameBuilder;
 import org.apache.pinot.util.TestUtils;
 import org.testng.annotations.AfterClass;
@@ -52,14 +59,18 @@ import static org.testng.Assert.assertTrue;
 public class UpsertTableSegmentPreloadIntegrationTest extends BaseClusterIntegrationTestSet {
   private static final int NUM_SERVERS = 1;
   private static final String PRIMARY_KEY_COL = "clientId";
-  private static final String REALTIME_TABLE_NAME = TableNameBuilder.REALTIME.tableNameWithType(DEFAULT_TABLE_NAME);
+  private static final String REALTIME_TABLE_NAME_1 = TableNameBuilder.REALTIME.tableNameWithType(DEFAULT_TABLE_NAME);
+  private static final String REALTIME_TABLE_NAME_2 = TableNameBuilder.REALTIME.tableNameWithType(DEFAULT_TABLE_NAME + "_2");
 
-  // Segment 1 contains records of pk value 100000 (partition 0)
-  private static final String UPLOADED_SEGMENT_1 = "mytable_10027_19736_0 %";
-  // Segment 2 contains records of pk value 100001 (partition 1)
-  private static final String UPLOADED_SEGMENT_2 = "mytable_10072_19919_1 %";
-  // Segment 3 contains records of pk value 100002 (partition 1)
-  private static final String UPLOADED_SEGMENT_3 = "mytable_10158_19938_2 %";
+
+  // Assume that we have segment data for the second table similar to the first table
+  private static final String UPLOADED_SEGMENT_1_TABLE_1 = "mytable_10027_19736_0 %";
+  private static final String UPLOADED_SEGMENT_2_TABLE_1 = "mytable_10072_19919_1 %";
+  private static final String UPLOADED_SEGMENT_3_TABLE_1 = "mytable_10158_19938_2 %";
+
+  private static final String UPLOADED_SEGMENT_1_TABLE_2 = "mytable_2_10027_19736_0 %";
+  private static final String UPLOADED_SEGMENT_2_TABLE_2 = "mytable_2_10072_19919_1 %";
+  private static final String UPLOADED_SEGMENT_3_TABLE_2 = "mytable_2_10158_19938_2 %";
 
   @BeforeClass
   public void setUp()
@@ -68,50 +79,85 @@ public class UpsertTableSegmentPreloadIntegrationTest extends BaseClusterIntegra
 
     // Start the Pinot cluster
     startZk();
-    // Start a customized controller with more frequent realtime segment validation
     startController();
     startBroker();
     startServers(NUM_SERVERS);
 
     // Start Kafka and push data into Kafka
     startKafka();
-
-    populateTables();
-  }
-
-  protected void populateTables()
-      throws Exception {
     // Unpack the Avro files
     List<File> avroFiles = unpackAvroData(_tempDir);
+    pushAvroIntoKafka(avroFiles);
+
+    populateTable(REALTIME_TABLE_NAME_1, avroFiles);
+    populateTable(REALTIME_TABLE_NAME_2, avroFiles);
+  }
+
+  protected void populateTable(String tableName, List<File> avroFiles)
+      throws Exception {
+    TestUtils.ensureDirectoriesExistAndEmpty(_segmentDir, _tarDir);
+
 
     // Create and upload schema and table config
     Schema schema = createSchema();
     addSchema(schema);
     TableConfig tableConfig =
-        createUpsertTableConfig(avroFiles.get(0), PRIMARY_KEY_COL, null, getNumKafkaPartitions());
+        createUpsertTableConfig(tableName, avroFiles.get(0), PRIMARY_KEY_COL, null, getNumKafkaPartitions());
     tableConfig.getUpsertConfig().setEnablePreload(true);
     tableConfig.getUpsertConfig().setEnableSnapshot(true);
     addTableConfig(tableConfig);
 
     // Create and upload segments
     ClusterIntegrationTestUtils.buildSegmentsFromAvro(avroFiles, tableConfig, schema, 0, _segmentDir, _tarDir);
-    uploadSegments(getTableName(), TableType.REALTIME, _tarDir);
+    uploadSegments(tableName, TableType.REALTIME, _tarDir);
 
-    pushAvroIntoKafka(avroFiles);
     // Wait for all documents loaded
-    waitForAllDocsLoaded(600_000L);
+    waitForAllDocsLoaded(tableName, 600_000L);
+  }
+
+  protected TableConfig createUpsertTableConfig(String tableName, File sampleAvroFile, String primaryKeyColumn,
+      String deleteColumn, int numPartitions) {
+    AvroFileSchemaKafkaAvroMessageDecoder._avroFile = sampleAvroFile;
+    Map<String, ColumnPartitionConfig> columnPartitionConfigMap = new HashMap<>();
+    columnPartitionConfigMap.put(primaryKeyColumn, new ColumnPartitionConfig("Murmur", numPartitions));
+
+    UpsertConfig upsertConfig = new UpsertConfig(UpsertConfig.Mode.FULL);
+    upsertConfig.setDeleteRecordColumn(deleteColumn);
+
+    return new TableConfigBuilder(TableType.REALTIME).setTableName(tableName).setSchemaName(getSchemaName())
+        .setTimeColumnName(getTimeColumnName()).setFieldConfigList(getFieldConfigs()).setNumReplicas(getNumReplicas())
+        .setSegmentVersion(getSegmentVersion()).setLoadMode(getLoadMode()).setTaskConfig(getTaskConfig())
+        .setBrokerTenant(getBrokerTenant()).setServerTenant(getServerTenant()).setIngestionConfig(getIngestionConfig())
+        .setLLC(useLlc()).setStreamConfigs(getStreamConfigs()).setNullHandlingEnabled(getNullHandlingEnabled())
+        .setRoutingConfig(new RoutingConfig(null, null, RoutingConfig.STRICT_REPLICA_GROUP_INSTANCE_SELECTOR_TYPE))
+        .setSegmentPartitionConfig(new SegmentPartitionConfig(columnPartitionConfigMap))
+        .setReplicaGroupStrategyConfig(new ReplicaGroupStrategyConfig(primaryKeyColumn, 1))
+        .setUpsertConfig(upsertConfig).build();
   }
 
   @Override
   protected void overrideServerConf(PinotConfiguration serverConf) {
     serverConf.setProperty(CommonConstants.Server.INSTANCE_DATA_MANAGER_CONFIG_PREFIX + ".max.segment.preload.threads",
         "1");
+    serverConf.setProperty(CommonConstants.Server.CONFIG_OF_INSTANCE_DATA_DIR, System.getProperty("user.dir") + "/MULTI_TABLE");
   }
 
   @AfterClass
   public void tearDown()
       throws IOException {
-    String realtimeTableName = TableNameBuilder.REALTIME.tableNameWithType(getTableName());
+    dropTable(REALTIME_TABLE_NAME_1);
+    dropTable(REALTIME_TABLE_NAME_2);
+    stopServer();
+    stopBroker();
+    stopController();
+    stopKafka();
+    stopZk();
+    FileUtils.deleteDirectory(_tempDir);
+  }
+
+  private void dropTable(String tableName)
+      throws IOException {
+    String realtimeTableName = TableNameBuilder.REALTIME.tableNameWithType(tableName);
 
     // Test dropping all segments one by one
     List<String> segments = listSegments(realtimeTableName);
@@ -129,12 +175,6 @@ public class UpsertTableSegmentPreloadIntegrationTest extends BaseClusterIntegra
     }, 60_000L, "Failed to drop the segments");
 
     dropRealtimeTable(realtimeTableName);
-    stopServer();
-    stopBroker();
-    stopController();
-    stopKafka();
-    stopZk();
-    FileUtils.deleteDirectory(_tempDir);
   }
 
   @Override
@@ -163,22 +203,22 @@ public class UpsertTableSegmentPreloadIntegrationTest extends BaseClusterIntegra
     return 3;
   }
 
-  @Override
-  protected void waitForAllDocsLoaded(long timeoutMs)
+  protected void waitForAllDocsLoaded(String tableName, long timeoutMs)
       throws Exception {
     TestUtils.waitForCondition(aVoid -> {
       try {
-        return getCurrentCountStarResultWithoutUpsert() == getCountStarResultWithoutUpsert();
+        return getCurrentCountStarResultWithoutUpsert(tableName) == getCountStarResultWithoutUpsert();
       } catch (Exception e) {
         return null;
       }
-    }, 100L, timeoutMs, "Failed to load all documents");
+    }, 5000L, timeoutMs, "Failed to load all documents");
     assertEquals(getCurrentCountStarResult(), getCountStarResult());
   }
 
-  private long getCurrentCountStarResultWithoutUpsert() {
-    return getPinotConnection().execute("SELECT COUNT(*) FROM " + getTableName() + " OPTION(skipUpsert=true)")
+  private long getCurrentCountStarResultWithoutUpsert(String tableName) {
+    long result = getPinotConnection().execute("SELECT COUNT(*) FROM " + tableName + " OPTION(skipUpsert=true)")
         .getResultSet(0).getLong(0);
+    return result;
   }
 
   private long getCountStarResultWithoutUpsert() {
@@ -189,27 +229,34 @@ public class UpsertTableSegmentPreloadIntegrationTest extends BaseClusterIntegra
   @Test
   public void testSegmentAssignment()
       throws Exception {
-    verifyIdealState(5);
-
-    // Run the real-time segment validation and check again
+    verifyIdealState(REALTIME_TABLE_NAME_1, 5);
     _controllerStarter.getRealtimeSegmentValidationManager().run();
-    verifyIdealState(5);
-    assertEquals(getCurrentCountStarResult(), getCountStarResult());
-    assertEquals(getCurrentCountStarResultWithoutUpsert(), getCountStarResultWithoutUpsert());
+    verifyIdealState(REALTIME_TABLE_NAME_1, 5);
+    assertEquals(getCurrentCountStarResult(REALTIME_TABLE_NAME_1), getCountStarResult());
+    assertEquals(getCurrentCountStarResultWithoutUpsert(REALTIME_TABLE_NAME_1), getCountStarResultWithoutUpsert());
+    waitForSnapshotCreation(REALTIME_TABLE_NAME_1);
 
-    waitForSnapshotCreation();
+    verifyIdealState(REALTIME_TABLE_NAME_2, 5);
+    _controllerStarter.getRealtimeSegmentValidationManager().run();
+    verifyIdealState(REALTIME_TABLE_NAME_2, 5);
+    assertEquals(getCurrentCountStarResult(REALTIME_TABLE_NAME_2), getCountStarResult());
+    assertEquals(getCurrentCountStarResultWithoutUpsert(REALTIME_TABLE_NAME_2), getCountStarResultWithoutUpsert());
+    waitForSnapshotCreation(REALTIME_TABLE_NAME_2);
 
-    // Restart the servers and check again
     restartServers();
-    verifyIdealState(7);
-    waitForAllDocsLoaded(600_000L);
+
+    verifyIdealState(REALTIME_TABLE_NAME_1, 7);
+    waitForAllDocsLoaded(REALTIME_TABLE_NAME_1, 600_000L);
+
+    verifyIdealState(REALTIME_TABLE_NAME_2, 7);
+    waitForAllDocsLoaded(REALTIME_TABLE_NAME_2, 600_000L);
   }
 
-  protected void waitForSnapshotCreation()
+  protected void waitForSnapshotCreation(String tableName)
       throws Exception {
-    Set<String> consumingSegments = getConsumingSegmentsFromIdealState(getTableName() + "_REALTIME");
+    Set<String> consumingSegments = getConsumingSegmentsFromIdealState(tableName);
     // trigger force commit for snapshots
-    String jobId = forceCommit(getTableName());
+    String jobId = forceCommit(tableName);
 
     Set<String> finalConsumingSegments = consumingSegments;
 
@@ -217,14 +264,14 @@ public class UpsertTableSegmentPreloadIntegrationTest extends BaseClusterIntegra
       try {
         if (isForceCommitJobCompleted(jobId)) {
           assertTrue(_controllerStarter.getHelixResourceManager()
-              .getOnlineSegmentsFromIdealState(getTableName() + "_REALTIME", false)
+              .getOnlineSegmentsFromIdealState(tableName, false)
               .containsAll(finalConsumingSegments));
 
           int snapshotFileCount = 0;
           for (BaseServerStarter serverStarter : _serverStarters) {
             String segmentDir =
                 serverStarter.getConfig().getProperty(CommonConstants.Server.CONFIG_OF_INSTANCE_DATA_DIR);
-            File[] files = new File(segmentDir, getTableName() + "_REALTIME").listFiles();
+            File[] files = new File(segmentDir, tableName).listFiles();
             for (File file : files) {
               if (!file.getName().startsWith(getTableName())) {
                 continue;
@@ -249,8 +296,8 @@ public class UpsertTableSegmentPreloadIntegrationTest extends BaseClusterIntegra
     }, 120000L, "Error verifying force commit operation on table!");
   }
 
-  protected void verifyIdealState(int numSegmentsExpected) {
-    IdealState idealState = HelixHelper.getTableIdealState(_helixManager, REALTIME_TABLE_NAME);
+  protected void verifyIdealState(String tableName, int numSegmentsExpected) {
+    IdealState idealState = HelixHelper.getTableIdealState(_helixManager, tableName);
     Map<String, Map<String, String>> segmentAssignment = idealState.getRecord().getMapFields();
     assertEquals(segmentAssignment.size(), numSegmentsExpected);
 
@@ -336,10 +383,13 @@ public class UpsertTableSegmentPreloadIntegrationTest extends BaseClusterIntegra
 
   private static int getSegmentPartitionId(String segmentName) {
     switch (segmentName) {
-      case UPLOADED_SEGMENT_1:
+      case UPLOADED_SEGMENT_1_TABLE_1:
+      case UPLOADED_SEGMENT_1_TABLE_2:
         return 0;
-      case UPLOADED_SEGMENT_2:
-      case UPLOADED_SEGMENT_3:
+      case UPLOADED_SEGMENT_2_TABLE_1:
+      case UPLOADED_SEGMENT_3_TABLE_1:
+      case UPLOADED_SEGMENT_2_TABLE_2:
+      case UPLOADED_SEGMENT_3_TABLE_2:
         return 1;
       default:
         return new LLCSegmentName(segmentName).getPartitionGroupId();
